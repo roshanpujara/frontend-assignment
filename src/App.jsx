4023d4d44324b74746a17d5b0f5e411db9b34cbf
roshan pujara <pujararoshan984@gmail.com>
2023-07-21 01:25:41 +0545

    useQuery setup using react-query(tanstack)

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
+import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <>
-      <Home />
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/*" element={<h1>pagenotfound</h1>} />
+        </Routes>
+      </QueryClientProvider>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { API_URL } from "../config/constants";
+
 const Home = () => {
+  const { data, isLoading, error } = useQuery(["productsList"], async () => {
+    const response = await axios.get(API_URL);
+    return response.data;
+  });
+  console.log(data);
+  if (isLoading) {
+    return <div>loading...</div>;
+  }
+  if (error) {
+    return <div>heyerror {error.message}</div>;
+  }
   return <div className="bg-green-100">Home</div>;
 };
 
