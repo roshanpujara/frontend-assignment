a16861eae2c11bef6a2aa72f49fefe5c728e3c04
roshan pujara <pujararoshan984@gmail.com>
2023-07-21 12:00:25 +0545

    data fetched and display in homepage

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,50 +1,38 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 export const Products = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setProducts(data);
+      console.log(products);
+    };
+    fetchProducts();
+  }, []);
   return (
-    <div className="flex flex-wrap justify-center gap-1">
-      <div className="w-3/12 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img
-            className="rounded-t-lg"
-            src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg"
-            alt=""
-          />
-        </a>
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Add to cart
-          </button>
-        </div>
-      </div>
-      <div className="w-3/12 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
-          <img
-            className="rounded-t-lg"
-            src="https://static.vecteezy.com/system/resources/previews/004/299/835/original/online-shopping-on-phone-buy-sell-business-digital-web-banner-application-money-advertising-payment-ecommerce-illustration-search-free-vector.jpg"
-            alt=""
-          />
-        </a>
-        <div className="p-5">
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
-          </h5>
-
-          <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-            Here are the biggest enterprise technology acquisitions of 2021 so
-            far, in reverse chronological order.
-          </p>
-          <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Add to cart
-          </button>
-        </div>
-      </div>
+    <div className="productsWrapper">
+      {products.length === 0 ? (
+        <span style={{ color: "red" }}>loading...</span>
+      ) : (
+        products.map((value) => {
+          return (
+            <div className="card" key={value.id}>
+              <Link to={"/detail/" + value.id}>
+                <img src={value.image} alt="" />
+              </Link>
+              <div>
+                <p>{value.title}</p>
+                <span className="price">$ {value.price}</span>
+                <div>
+                  <button className="btn">Add to cart</button>
+                </div>
+              </div>
+            </div>
+          );
+        })
+      )}
     </div>
   );
 };
